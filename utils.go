d70755293022e75249b88677df1ea73b744bc763
hsandor <hrisandor@gmail.com>
2017-03-20 12:23:45 +0100

    minor optimization

--- utils.go
+++ utils.go
@@ -6,10 +6,7 @@ func calcIndentLevel(s string) int {
 		if c == 32 {
 			i += 1
 		} else if c == 9 {
-			i += 1
-			for i%8 != 0 {
-				i += 1
-			}
+			i = ((i + 8) / 8) * 8
 		} else if c != 13 {
 			break
 		}
